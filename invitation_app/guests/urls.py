0a7af076deaa3cad9c086401020099176f968b07
Marin Maslov <marin.maslov@ericsson.com>
2019-10-22 16:10:55 +0200

    Changed view function to view class

--- invitation_app/guests/urls.py
+++ invitation_app/guests/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from .views import get_invitations
+from .views import GuestView #, get_invitations
 
 urlpatterns = [
-    path('', get_invitations),
+    #path('', get_invitations),
+    path('guest/', GuestView.as_view()),
 ]
--- invitation_app/guests/views.py
+++ invitation_app/guests/views.py
@@ -1,14 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import TemplateView
+from django.views.generic import View, TemplateView
 
 from guests.models import Invitation, Guest
 from .forms import SlugForm
 
 
-#def get_invitations(request):
-
-
+'''
 def get_invitations(request):
     if request.method == 'POST':
         form = SlugForm(request.POST)
@@ -27,7 +25,28 @@ def get_invitations(request):
 
     form = SlugForm()
     return render(request, 'guests/templates/slug.html', {'form': form})
+'''
 
 
 
-#class GuestView(TemplateView):
+
+class GuestView(View):
+    def get(self, request, *args, **kwargs):
+        form = SlugForm()
+        return render(request, 'guests/templates/index.html', {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        if request.method == 'POST':
+            form = SlugForm(request.POST)
+            if form.is_valid():
+                slug = form.cleaned_data['slug']
+                invitation = Invitation.objects.filter(slug=slug).first()
+                guest = Guest.objects.filter(invitation = invitation.id).first()
+                context = {
+                    'slug': invitation.slug,
+                    'created': invitation.created,
+                    'confirmed': invitation.confirmed,
+                    'isFamily': invitation.isFamily,
+                    'guest': guest
+                }
+                return render(request, 'guests/templates/invitation.html', context)
