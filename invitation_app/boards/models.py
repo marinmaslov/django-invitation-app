8b330d8a468ae018298613ae86da8198be585eed
easmsma <marin.maslov@ericsson.com>
2019-11-04 14:26:36 +0100

    Fixano sranje

--- invitation_app/boards/models.py
+++ invitation_app/boards/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.utils.crypto import get_random_string
 import datetime
 
-from users.models import Profile
+from django.contrib.auth.models import User
 
 # Board Model
 class Board(models.Model):
@@ -11,7 +11,7 @@ class Board(models.Model):
     image = models.ImageField(default='default-boards.jpg', upload_to='static/img/boards/')
     date_created = models.DateTimeField(default=datetime.datetime.now())
     laste_modified = models.DateTimeField(default=datetime.datetime.now())
-    profile = models.ForeignKey('users.Profile', on_delete=models.CASCADE, blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
         return str(self.name)
